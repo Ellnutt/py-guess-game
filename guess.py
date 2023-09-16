14b969b5a24c03e6f914faeef2f3b7ee3f8a08ca
Ellnutt <ellisnathan1@gmail.com>
2023-09-16 23:30:15 +0100

    1

--- guess.py
+++ guess.py
@@ -6,7 +6,7 @@ while True:
         print ("guess is low, getting close!!")
         guess = int(input("Enter an integer from 1 to 99: "))
     elif guess > n:
-        print ("guess is high!")
+        print ("guess is high")
         guess = int(input("Enter an integer from 1 to 99: "))
     else:
         print ("you guessed it right! Bye!")
